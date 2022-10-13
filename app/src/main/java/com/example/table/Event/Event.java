5f8672a8ce7d99d5d7330a4a0a42265fb08472b2
Trefilov Dmitry <m1907202@edu.misis.ru>
2022-10-14 02:04:08 +0300

    added object Event

--- app/src/main/java/com/example/table/Event/Event.java
+++ app/src/main/java/com/example/table/Event/Event.java
@@ -1,5 +1,6 @@
 package com.example.table.Event;
 
+import java.util.ArrayList;
 import java.util.Date;
 
 
@@ -20,6 +21,8 @@ public class Event {
     private int MinNumberPeople;
 
 //    private Notification notification; // TODO check android.app.Notification
+
+    public Event() {}
 
     public Event(
         String name,
@@ -45,5 +48,22 @@ public class Event {
         this.MaxNumberPeople = MaxNumberPeople;
         this.MinNumberPeople = MinNumberPeople;
 //        this.notification = notification;
+    }
+
+
+    public Event getEventById(Integer id) {
+        // обращение к БД
+        Event event = new Event();
+        return event;
+    }
+
+    public ArrayList<Event> getAllEvent(Integer id) {
+        // обращение к БД
+        ArrayList<Event> events = new ArrayList<Event>();
+        return events;
+    }
+
+    public void setEvent2DB() {
+        // запись текущих данных в БД
     }
 }
