be7a5f7b55170e3cf7b6b841af4d5e7fb5d2ff78
alinkaD01 <87359259+alinkaD01@users.noreply.github.com>
2022-11-03 18:50:18 +0300

    Register step 2 xml

---
+++ app/src/main/java/com/example/table/RegisterStep2_activity.java
@@ -0,0 +1,14 @@
+package com.example.table;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegisterStep2_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_step2);
+    }
+}
