4e59f7f7c0e29e50d1d200f4da4d7d8900d6fd22
alinkaD01 <87359259+alinkaD01@users.noreply.github.com>
2022-11-10 21:24:23 +0300

    start developing navigationButtom activity and profile

---
+++ app/src/main/java/com/example/table/ui/profile/ProfileViewModel.java
@@ -0,0 +1,7 @@
+package com.example.table.ui.profile;
+
+import androidx.lifecycle.ViewModel;
+
+public class ProfileViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/example/table/ui/search/SearchViewModel.java
@@ -0,0 +1,19 @@
+package com.example.table.ui.search;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class SearchViewModel extends ViewModel {
+
+    private final MutableLiveData<String> mText;
+
+    public SearchViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is home fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
