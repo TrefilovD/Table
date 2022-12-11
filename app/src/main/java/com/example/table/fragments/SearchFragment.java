27cf0b2682474637e616d2a6fd13516823cbbfce
Alex <alexandro-tolstenko@mail.ru>
2022-12-11 18:42:40 +0300

    Доделал все окна, навел красоту в старых, пофиксил пару старых окошек

--- app/src/main/java/com/example/table/fragments/SearchFragment.java
+++ app/src/main/java/com/example/table/fragments/SearchFragment.java
@@ -63,4 +63,6 @@ public class SearchFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_search, container, false);
     }
+
+
 }
