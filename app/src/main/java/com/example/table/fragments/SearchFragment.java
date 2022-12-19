ed684fd571c670be31bb3b596a0e9987c9e1be98
LizaAndVeta <m1901985@edu.misis.ru>
2022-12-19 21:09:55 +0300

    adding search in database

--- app/src/main/java/com/example/table/fragments/SearchFragment.java
+++ app/src/main/java/com/example/table/fragments/SearchFragment.java
@@ -2,13 +2,34 @@ package com.example.table.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 import com.example.table.R;
+import com.example.table.TableApp;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+import java.util.Map;
+
+import io.appwrite.Query;
+import io.appwrite.exceptions.AppwriteException;
+import io.appwrite.models.Document;
+import io.appwrite.models.DocumentList;
+import io.appwrite.services.Databases;
+import kotlin.Result;
+import kotlin.coroutines.Continuation;
+import kotlin.coroutines.CoroutineContext;
+import kotlin.coroutines.EmptyCoroutineContext;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -16,6 +37,7 @@ import com.example.table.R;
  * create an instance of this fragment.
  */
 public class SearchFragment extends Fragment {
+    TableApp myApp;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -55,6 +77,82 @@ public class SearchFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+    }
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        myApp = (TableApp) getActivity().getApplicationContext();
+        ImageButton button = (ImageButton) view.findViewById(R.id.lupa_button);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                try {
+                    onClick_new(view);
+                } catch (AppwriteException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    public void onClick_new(View view) throws AppwriteException {
+        //подключение к БД
+        Databases databases = new Databases(this.myApp.appwriteClient);
+        EditText search_editText = (EditText) getView().findViewById(R.id.search_editText);
+        String search_string = search_editText.getText().toString();
+        //запрос к БД
+        databases.listDocuments(
+                myApp.databaseID,
+                myApp.event_collectionID,
+                List.of(
+                        //поиск по индексу userID
+                        Query.Companion.search("name", search_string)
+                ),
+                //корутина для ожидания ответа с сервера
+                new Continuation<Object>() {
+                    @NotNull
+                    @Override
+                    public CoroutineContext getContext() {
+                        return EmptyCoroutineContext.INSTANCE;
+                    }
+
+                    //метод, который срабатывает после получения ответа с сервера
+                    @Override
+                    public void resumeWith(@NotNull Object o) { //о - ответ сервера в формате JSON
+                        try {
+                            if (o instanceof Result.Failure) {
+                                Result.Failure failure = (Result.Failure) o;
+                                throw failure.exception;
+                            } else {
+                                Log.d("Appwrite", o.toString());
+
+                                //ответ с сервера парсим в список документов (реализован в AppWrite)
+                                //в логах будет строчка с ответом с сервера
+                                DocumentList docs = (DocumentList) o;
+                                //достаем первый документ из полученных
+                                Document doc = docs.getDocuments().get(0);
+                                //получаем данные из БД
+                                Map<String, Object> response = doc.getData();
+                                myApp.personalData = response;
+
+                                //менять UI нужно в спец потоке (во фрагменте)
+                                getActivity().runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        FillPersonalData(view, response);
+                                    }
+                                });
+
+                            }
+                        } catch (Throwable th) {
+                            Log.e("PERSONAL_ACTIVITY_ERROR", th.toString());
+                        }
+                    }
+                }
+        );
+    }
+    public void FillPersonalData(View view, Map<String, Object> personalData) {
+
     }
 
     @Override
