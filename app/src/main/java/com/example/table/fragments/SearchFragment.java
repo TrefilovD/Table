9724edf99f4413319d9375ce55c00ae11cc043e1
Alex <alexandro-tolstenko@mail.ru>
2022-12-19 23:25:29 +0300

    связал классы мероприятий Димы с окном поиска

--- app/src/main/java/com/example/table/fragments/SearchFragment.java
+++ app/src/main/java/com/example/table/fragments/SearchFragment.java
@@ -1,9 +1,12 @@
 package com.example.table.fragments;
 
+import android.app.ApplicationErrorReport;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -12,7 +15,11 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageButton;
 
+import com.example.table.CreateEvent;
+import com.example.table.Event.Event;
 import com.example.table.R;
+import com.example.table.RegisterStep2_activity;
+import com.example.table.event;
 
 public class SearchFragment extends Fragment {
 
@@ -55,14 +62,43 @@ public class SearchFragment extends Fragment {
         button.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
+                onClick_new(view);
+            }
+        });
+        //кнопка для перехода к созданию мероприятия
+        Button create_event_btn = (Button) view.findViewById(R.id.buttom_createevent);
+        create_event_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onCreateEventBtn(view);
+            }
+        });
 
-                onClick_new();
+        //кнопка перехода к записи на мероприятие
+        Button register_on_event = (Button) view.findViewById(R.id.zapisatsya_Button_cardOfevent_1);
+        register_on_event.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                register_on_event_1_btn();
             }
         });
     }
 
-    public void onClick_new(){
+    public void onClick_new(View view){
         //
+
+        CardView cardView = (CardView) view.findViewById(R.id.card1);
+        cardView.setVisibility(View.INVISIBLE);
     }
 
+    public void onCreateEventBtn(View view){
+        Intent intent = new Intent(getActivity(), CreateEvent.class);
+        startActivity(intent);
+    }
+
+    public void register_on_event_1_btn(){
+        Intent intent = new Intent(getActivity(), event.class);
+        intent.putExtra("eventid", "635af8d3091b24e06a93");
+        startActivity(intent);
+    }
 }
