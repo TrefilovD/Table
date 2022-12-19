228b8ae6ef457e0a2b78cf9f649d4e750ce7c727
Trefilov Dmitry <m1907202@edu.misis.ru>
2022-12-19 04:20:34 +0300

    add some types of event's activities

--- app/src/main/java/com/example/table/EventEdit.java
+++ app/src/main/java/com/example/table/EventEdit.java
@@ -1,160 +1,152 @@
 package com.example.table;
+import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.Spinner;
 import android.widget.TextView;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import com.example.table.Event.Event;
 
-import org.jetbrains.annotations.NotNull;
-import org.json.JSONObject;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import io.appwrite.Query;
-import io.appwrite.exceptions.AppwriteException;
-import io.appwrite.models.Document;
-import io.appwrite.models.DocumentList;
 import io.appwrite.services.Databases;
+import org.jetbrains.annotations.NotNull;
+
+import io.appwrite.exceptions.AppwriteException;
 import kotlin.Result;
 import kotlin.coroutines.Continuation;
 import kotlin.coroutines.CoroutineContext;
 import kotlin.coroutines.EmptyCoroutineContext;
+//import io.appwrite.coroutines.CoroutineCallback;
+
 
 public class EventEdit extends AppCompatActivity {
-    Integer EventId;
     private TableApp myApp;
-
-    TextView name_tv;
-    TextView minUsersQty_tv;
-    TextView maxUsersQty_tv;
-    TextView dateTime_tv;
-    Spinner spinner_town;
-    TextView geo_tv;
-    Spinner spinner_metro;
-    TextView description_tv;
-    TextView linkToChat_tv;
-    TextView details_tv;
-    TextView price_tv;
-
-    Button button;
+    private Databases databases;
+    private String EventId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        try {
-            getEventInfo();
-        } catch (AppwriteException e) {
-            e.printStackTrace();
-        }
-
-        button = (Button) findViewById(R.id.buttonCreateEvent);
-        button.setText("Сохранить");
-
-        name_tv = (TextView) findViewById(R.id.editNameEvent);
-        minUsersQty_tv = (TextView) findViewById(R.id.editMinKolvo);
-        maxUsersQty_tv = (TextView) findViewById(R.id.editMaxKolvo);
-        dateTime_tv = (TextView) findViewById(R.id.editDate);
-        spinner_town = (Spinner) findViewById(R.id.spinner_gorod);
-        geo_tv = (TextView) findViewById(R.id.editPlace);
-        spinner_metro = (Spinner) findViewById(R.id.spinner_metro);
-        description_tv = (TextView) findViewById(R.id.editDescription);
-        linkToChat_tv = (TextView) findViewById(R.id.editChat);
-        details_tv = (TextView) findViewById(R.id.editDetali);
-//        price_tv = (TextView) findViewById(R.id.);
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.create_event);
+        Button save = (Button) findViewById(R.id.buttonCreateEvent);
+        save.setText("Сохранить");
+        myApp = (TableApp) getApplicationContext();
+        databases = new Databases(this.myApp.appwriteClient);
+        Intent intent = getIntent();
+        EventId = intent.getStringExtra("eventid");
     }
 
-    public void getEventInfo() throws AppwriteException {
-        Databases databases = new Databases(this.myApp.appwriteClient);
-        databases.listDocuments(
-                myApp.databaseID,
-                myApp.event_collectionID,
-                List.of(
-                        Query.Companion.equal("userID", EventId.toString())
-                ),
-                new Continuation<Object>() {
-                    @NotNull
-                    @Override
-                    public CoroutineContext getContext() {
-                        return EmptyCoroutineContext.INSTANCE;
-                    }
+    public void onClickSaveEvent(View view) throws AppwriteException {
+        TextView name_tv = (TextView) findViewById(R.id.editNameEvent);
+        TextView minUsersQty_tv = (TextView) findViewById(R.id.editMinKolvo);
+        TextView maxUsersQty_tv = (TextView) findViewById(R.id.editMaxKolvo);
+        TextView price_tv = (TextView) findViewById(R.id.priceTextView);
+        CheckBox price_box = (CheckBox) findViewById(R.id.checkbox_free);
+        TextView dateTime_tv = (TextView) findViewById(R.id.editDate);
+        TextView editTimeEnd_tv = (TextView) findViewById(R.id.editTimeEnd);
+        TextView editTimeBegin_tv = (TextView) findViewById(R.id.editTimeBegin);
+        Spinner spinner_town = (Spinner) findViewById(R.id.spinner_gorod);
+        TextView geo_tv = (TextView) findViewById(R.id.editPlace);
+        Spinner spinner_metro = (Spinner) findViewById(R.id.spinner_metro);
+        TextView description_tv = (TextView) findViewById(R.id.editDescription);
+        TextView linkToChat_tv = (TextView) findViewById(R.id.editChat);
+        TextView details_tv = (TextView) findViewById(R.id.editDetali);
+        TextView hashtags_tv = (TextView) findViewById(R.id.hashtag_event);
 
-                    @Override
-                    public void resumeWith(@NotNull Object o) {
-                        try {
-                            if (o instanceof Result.Failure) {
-                                Result.Failure failure = (Result.Failure) o;
-                                throw failure.exception;
-                            } else {
-                                Log.d("Appwrite", o.toString());
-                                DocumentList docs = (DocumentList) o;
-                                Document doc = docs.getDocuments().get(0);
-                                Map<String, Object> response = doc.getData();
-                                FillEventData(response);
-                                /*Gson g = new Gson();
-                                Log.d("UserData", docs.getDocuments().get(0).getData().toString());
-                                JsonElement jsonElement = g.toJsonTree(docs.getDocuments().get(0).getData());
-                                Log.i("INFO", "ACCESS_USER_DATA");
-                                myApp.personalData = g.fromJson(jsonElement, UserData.class);
-                                Log.i("INFO", "CHANGES");
-                                FillPersonalData(myApp.personalData);*/
-                            }
-                        } catch (Throwable th) {
-                            Log.e("PERSONAL_ACTIVITY_ERROR", th.toString());
-                        }
-                    }
-                }
+
+        String name = name_tv.getText().toString();
+        Integer minUsersQty, maxUsersQty;
+        try {
+            minUsersQty = Integer.parseInt(minUsersQty_tv.getText().toString());
+            maxUsersQty = Integer.parseInt(maxUsersQty_tv.getText().toString());
+        }
+        catch (NumberFormatException e) {
+            Log.e("Value error", "Mistakes in min/max users");
+            return;
+        }
+        Date dateTime, editTimeEnd, editTimeBegin;
+        String str_date = dateTime_tv.getText().toString();
+        DateFormat format = new SimpleDateFormat("dd.MM.yyyy", Locale.ENGLISH);
+        try {
+            dateTime = format.parse(str_date);
+        }
+        catch (ParseException e) {
+            Log.e("Value error", "Mistakes in date");
+            return;
+        }
+        String editTimeEnd_str = editTimeEnd_tv.getText().toString();
+        String editTimeBegin_str = editTimeBegin_tv.getText().toString();
+        format = new SimpleDateFormat("hh:mm", Locale.ENGLISH);
+        try {
+            editTimeEnd = format.parse(editTimeEnd_str);
+            editTimeBegin = format.parse(editTimeBegin_str);
+        }
+        catch (ParseException e) {
+            Log.e("Value error", "Mistakes in date");
+            return;
+        }
+        Double price = 0.;
+        if (!price_box.isChecked()) {
+            price = Double.parseDouble(price_tv.getText().toString());
+        }
+
+        if (minUsersQty >= maxUsersQty) {
+            minUsersQty = maxUsersQty;
+            Log.w("MIN more MAX", "MIN = MAX");
+        }
+
+        String city = spinner_town.getSelectedItem().toString();
+        String geo = geo_tv.getText().toString();
+        String[] metro = new String[] {spinner_metro.getSelectedItem().toString()};
+        String description = description_tv.getText().toString();
+        String linkToChat = linkToChat_tv.getText().toString();
+        String details = details_tv.getText().toString();
+        String[] hashtags = hashtags_tv.getText().toString().
+                replaceFirst("#", "").split("#");
+        String status = "created";
+        String hostID = myApp.userID;
+        String[] participants = new String[] {myApp.userID};
+
+        Event event = new Event(
+                name,
+                minUsersQty,
+                maxUsersQty,
+                dateTime.toString(),
+                editTimeEnd.toString(),
+                editTimeBegin.toString(),
+                city,
+                geo,
+                metro,
+                description,
+                linkToChat,
+                details,
+                hashtags,
+                status,
+                hostID,
+                participants,
+                price
         );
+
+        updateEvent(event);
     }
 
-    public void FillEventData(Map<String, Object> event) throws AppwriteException {
+    private void updateEvent(Event event) throws AppwriteException {
 
-        String[] participants = (String []) event.get("participants");
-        String[] hashtags = (String []) event.get("hashtags");
-
-        name_tv.setText(event.get("name").toString());
-        minUsersQty_tv.setText(event.get("minUsersQty").toString());
-        maxUsersQty_tv.setText(event.get("maxUsersQty").toString());
-        dateTime_tv.setText(event.get("dateTime").toString());
-//        spinner_town.setSe(event.get("dateTime").toString());
-        geo_tv.setText(event.get("geo").toString());
-//        spinner_metro = (Spinner) findViewById(R.id.spinner_metro);
-        description_tv.setText(event.get("description").toString());
-        linkToChat_tv.setText(event.get("linkToChat").toString());
-        details_tv.setText(event.get("details").toString());
-        onClickSaveChanges(event);
-    }
-
-    public void onClickEventInfo(View view) {
-
-    }
-
-    public void onClickEditEvent(View view) {
-
-    }
-
-    public void onClickSaveChanges(Map<String, Object> event) throws AppwriteException {
-        // обновление БД
-
-        String id = "asdasdasd";
-
-        Intent intent = new Intent(this, event.class);
-        Databases databases = new Databases(myApp.appwriteClient);
         databases.updateDocument(
                 myApp.databaseID,
                 myApp.event_collectionID,
-                event.get("id").toString(),
-                new JSONObject(event),
+                "unique()",
+                event,
                 new Continuation<Object>() {
                     @NotNull
                     @Override
@@ -169,18 +161,12 @@ public class EventEdit extends AppCompatActivity {
                                 Result.Failure failure = (Result.Failure) o;
                                 throw failure.exception;
                             } else {
-//                                io.appwrite.models.Account account = (io.appwrite.models.Account) o;
-//                                Log.i("CREATING EVENT SUCCESS", account.toString());
-                                button.setText("Создать");
-                                intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
-                                startActivity(intent);
+                                moveOnEventHost();
                             }
                         } catch (Throwable th) {
                             Log.e("CREATING EVENT ERROR", th.toString());
                             if (th.toString().contains("A user with the same email already exists in your project")) {
                                 Log.e("CREATING EVENT ERROR", "A user with the same email already exists in your project");
-                                // Выводим сообщение что аккаунт уже существует
-//                                throw new IllegalArgumentException();
                             }
                         }
                     }
@@ -188,4 +174,14 @@ public class EventEdit extends AppCompatActivity {
         );
     }
 
+    private void moveOnEventHost() {
+//        String hash = new ID().toString();
+        Button save = (Button) findViewById(R.id.buttonCreateEvent);
+        save.setText("Создать");
+        Log.i("SUPER", "Super");
+        Intent intent = new Intent(this, event.class);
+        intent.putExtra("eventid", EventId);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+        startActivity(intent);
+    }
 }
